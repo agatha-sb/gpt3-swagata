e35276cc5473cdd417591137d55aebbcd653e143
swagata-codearts <swagata.codearts@gmail.com>
2022-11-06 23:16:42 +0530

    add: Navigation links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,12 +3,21 @@ import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
 import  logo from '../../assets/logo.svg'
 import './navbar.css'
 
+// BEM -> Block Element Modifier
+
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt='logo' />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <a href="#home">Home</a>
+          <a href="#wgpt3">What is GPT?</a>
+          <a href="#possibility">Open AI</a>
+          <a href="#features">Case Studies</a>
+          <a href="#blog">Library</a>
         </div>
       </div>
     </div>
